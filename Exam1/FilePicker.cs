69165d75e16e4f7979b6a32feaa4ae3245a9190e
Dmytro <karambolrul@gmail.com>
2020-06-06 13:01:29 +0300

    Add project files.

---
+++ Exam1/FilePicker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1
+{
+    class FilePicker
+    {
+        public string FirstFilePath;
+        public string SecondFilePath;
+        public FilePicker(string filePath1, string filePath2)
+        {
+            FirstFilePath = filePath1;
+            SecondFilePath = filePath2;
+        }
+        public void PickBiggerNums(int number)
+        {
+            List<int> selected = new List<int>();
+            BinaryReader reader = new BinaryReader(new FileStream(FirstFilePath, FileMode.OpenOrCreate));
+            int currentElement;
+            while(reader.BaseStream.Position != reader.BaseStream.Length)
+            {
+                currentElement = reader.ReadInt32();
+                if(currentElement >= number)
+                {
+                    selected.Add(currentElement);
+                }
+            }
+            reader.Close();
+            reader = new BinaryReader(new FileStream(SecondFilePath, FileMode.OpenOrCreate));
+            while (reader.BaseStream.Position != reader.BaseStream.Length)
+            {
+                currentElement = reader.ReadInt32();
+                if (currentElement >= number)
+                {
+                    selected.Add(currentElement);
+                }
+            }
+            reader.Close();
+            BinaryWriter writer = new BinaryWriter(new FileStream("output.txt", FileMode.OpenOrCreate));
+            foreach(int num in selected)
+            {
+                Console.WriteLine(num);
+                writer.Write(num);
+            }
+            writer.Close();
+        }
+    }
+}
---
+++ Exam1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exam1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryWriter writer = new BinaryWriter(new FileStream("binary1.txt", FileMode.OpenOrCreate));
+            writer.Write(230);
+            writer.Write(130);
+            writer.Write(50);
+            writer.Close();
+            writer = new BinaryWriter(new FileStream("binary2.txt", FileMode.OpenOrCreate));
+            writer.Write(20);
+            writer.Write(250);
+            writer.Write(20);
+            writer.Write(50);
+            writer.Close();
+            FilePicker picker = new FilePicker("binary1.txt", "binary2.txt");
+            picker.PickBiggerNums(123);
+            Console.ReadKey();
+        }
+    }
+}
